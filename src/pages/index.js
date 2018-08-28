9d0bb69d5c5f158ac3fe28e6fd763e4b6b85b440
sabihashaik <sabihaabc1@gmail.com>
2018-08-28 23:54:20 +0400

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Hi! I'm sabiha, a 3rd year undergrad studying Computer Science. 
+      I love exploring and learning new technologies which is one of the reasons 
+      I'm excited to be a part of this hackathon!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
